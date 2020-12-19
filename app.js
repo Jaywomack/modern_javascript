e83afc8efe7df7e49a8e85bf9805fd9f41165f41
Jay Womack <womackjw@hotmail.com>
2020-12-19 12:14:44 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+/***
+ *
+ *STRING METHODS
+ */
+// const firstName = 'Jared';
+// const lastName = 'Womack';
+// const str = 'Hello there my name is Jared';
+// const tags = 'web design, photography, design';
+
+// val = firstName + lastName;
+
+// // Length
+// val = firstName.length;
+
+// // concat
+// val = firstName.concat(' ', lastName);
+
+// // Change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
+
+// // By index, 0 based
+// val = firstName[0];
+
+// // indexOf()
+// val = firstName.indexOf('J');
+
+// // lastIndexOf()
+// val = firstName.lastIndexOf('d');
+
+// // Char at
+// val = lastName.charAt('2'); // Will return -1 if not present
+
+// // get last char at
+// val = firstName.charAt(firstName.length - 1);
+
+// // substring
+// val = firstName.substring(0, 3);
+
+// // slice()
+// val = firstName.slice(0, 4);
+// val = firstName.slice(-3); // Will take the last three of the string
+
+// //split()
+// val = str.split(' ');
+// val = tags.split(',');
+
+// //replace()
+// val = str.replace('Jared', 'Jay');
+
+// // includes()
+// val = str.includes('Hello');
+
+// console.log(val);
+
+/***
+ *
+ * TEMPLATE LITERALS
+ */
