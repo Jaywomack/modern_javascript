4b26771c4b687a3d1dc5cc4cb13ea93711207128
jaywomack <womackjw@hotmail.com>
2020-12-21 14:16:27 -0800

    Created dom variables and add task function

--- tasklist3/app.js
+++ tasklist3/app.js
@@ -1,1 +1,41 @@
+// create DOM variables
+const app = document.querySelector('.app');
+const taskInput = document.getElementById('task');
+const form = document.getElementById('form');
+const addTaskButton = document.querySelector('.task-btn');
+const collection = document.querySelector('.collection');
+const clearAllButton = document.querySelector('.clear-all');
 
+// call load events function
+loadEventListeners();
+// load all the event listeners
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+// add task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Please Enter a Task.');
+  } else {
+    // create li
+    const li = document.createElement('li');
+    // append text node to li
+    li.append(document.createTextNode(taskInput.value));
+    // add classes to li
+    li.classList =
+      'list-group-item d-flex justify-content-between align-items-center text-dark bg-light my-1';
+    // create span
+    const span = document.createElement('span');
+    // add text content to span
+    span.append(document.createTextNode('X'));
+    // add classes to span
+    span.classList = 'badge badge-danger badge-pill p-2';
+    // append span to li
+    li.appendChild(span);
+    // append li to collection
+    collection.appendChild(li);
+    // prevent default behavior
+    e.preventDefault();
+  }
+}
