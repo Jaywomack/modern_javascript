2e6853aef56470685dba14d5084e84ac3350d8b1
jaywomack <womackjw@hotmail.com>
2020-12-21 19:29:30 -0800

    hide token

--- forecaster/app.js
+++
@@ -1,8 +0,0 @@
-const { default: Axios } = require("axios");
-
-requires Axios
-let url =
-  'api.openweathermap.org/data/2.5/weather?q={city name}&appid={c3aefd6ca6d654b410a60a3855d20345}';
-axios.get(url).then((response) => {
-  console.log(response.data);
-});
